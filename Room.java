a81ff45d78ae6b436671e4e391dccc232cc1c6cd
NTE ADMIN <mawayalebo@gmail.com>
2022-05-05 12:37:03 +0200

    class Room with method

---
+++ Room.java
@@ -0,0 +1,10 @@
+//class "Room"
+class Room {
+    private int height;
+    private int width;
+    private int breadth;
+    //volume method
+    public int volume() {
+        return height * width * breadth;
+    }
+}
